d117f7b964b12fe45ce7caa5c16a9d8fd050bd76
pricope-marius-andrei <pricopemarius33@gmail.com>
2024-11-04 20:28:49 +0200

    resolve migration

--- Infrastracture/DependancyInjection.cs
+++ Infrastracture/DependancyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Domain.Interfaces;
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Domain.Repositories;
 using Infrastracture.Persistence;
 using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -14,13 +16,13 @@ namespace Infrastracture
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(
                     configuration.GetConnectionString("DefaultConnection"),
-                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+                    b => b.MigrationsAssembly("Predictive-Healthcare-Management-System")));
 
-            /*services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IPatientRepository, PatientRepository>();
             services.AddScoped<IDoctorRepository, DoctorRepository>();
             services.AddScoped<IMedicalHistoryRepository, MedicalHistoryRepository>();
-            services.AddScoped<IRecordRepository, RecordRepository>();*/
+            //services.AddScoped<IRecordRepository, RecordRepository>();
 
             return services;
         }
