9be12965235464ca56b4e089e268b584c3f3ed62
pricope-marius-andrei <pricopemarius33@gmail.com>
2024-11-12 13:43:30 +0200

    refactor code

--- Application/UseCases/Commands/CreateDoctorCommand.cs
+++ Application/UseCases/Commands/CreateDoctorCommand.cs
@@ -1,18 +1,19 @@
 ﻿using Domain.Common;
+using Domain.Entities;
 using MediatR;
 
 namespace Application.UseCases.Commands
 {
     public class CreateDoctorCommand : IRequest<Result<Guid>>
     {
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Specialization { get; set; }
-        public string PhoneNumber { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Specialization { get; set; }
+        public required string PhoneNumber { get; set; }
         public DateTime DateOfRegistration { get; set; }
-
+        public required List<MedicalRecord> MedicalRecords { get; set; }
     }
 }
