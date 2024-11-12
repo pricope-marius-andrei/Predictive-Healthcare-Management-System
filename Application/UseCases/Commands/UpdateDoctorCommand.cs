3b9014012d03df8316ee166035dc9a864a5b8187
pricope-marius-andrei <pricopemarius33@gmail.com>
2024-11-12 10:05:02 +0200

    add required keyword for Doctor's fields

--- Application/UseCases/Commands/UpdateDoctorCommand.cs
+++ Application/UseCases/Commands/UpdateDoctorCommand.cs
@@ -6,13 +6,13 @@ namespace Application.UseCases.Commands
 {
     public class UpdateDoctorCommand : IRequest<Result<Doctor>>
     {
-        public Guid DoctorId { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Specialization { get; set; }
-        public string PhoneNumber { get; set; }
+        public required Guid DoctorId { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Specialization { get; set; }
+        public required string PhoneNumber { get; set; }
     }
 }
--- Domain/Entities/Doctor.cs
+++ Domain/Entities/Doctor.cs
@@ -8,28 +8,28 @@ namespace Domain.Entities
         public Guid DoctorId { get; set; }
 
         [Required]
-        public string Username { get; set; }
+        public required string Username { get; set; }
 
         [Required]
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
         [Required]
-        public string Password { get; set; }
+        public required string Password { get; set; }
 
         [Required]
-        public string FirstName { get; set; }
+        public required string FirstName { get; set; }
 
         [Required]
-        public string LastName { get; set; }
+        public required string LastName { get; set; }
 
         [Required]
-        public string PhoneNumber { get; set; }
+        public required string PhoneNumber { get; set; }
 
         [Required]
-        public string Specialization { get; set; }
+        public required string Specialization { get; set; }
 
         public DateTime DateOfRegistration { get; set; } = DateTime.Now;
 
-        public ICollection<MedicalRecord> MedicalRecords { get; set; }
+        public required ICollection<MedicalRecord> MedicalRecords { get; set; }
     }
 }
