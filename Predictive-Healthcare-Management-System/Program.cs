d211ab68a044da5bfd64c4628d0c8daf155a9df0
pricope-marius-andrei <pricopemarius33@gmail.com>
2024-12-02 22:32:16 +0200

    resolve versions problems

--- Predictive-Healthcare-Management-System/Program.cs
+++ Predictive-Healthcare-Management-System/Program.cs
@@ -3,10 +3,25 @@ using Predictive_Healthcare_Management_System;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") // Allow Angular app origin
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
 
+
+
 var app = builder.Build();
+
+// Use CORS policy
+app.UseCors("AllowAngularApp");
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
@@ -20,5 +35,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 await app.RunAsync();
+
+
 
 public partial class Program {}
