d28f43bf95b8041e5014d457411e1925819cb11b
pricope-marius-andrei <pricopemarius33@gmail.com>
2024-12-03 10:49:28 +0200

    add new origin

--- Predictive-Healthcare-Management-System/Program.cs
+++ Predictive-Healthcare-Management-System/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp", policy =>
     {
-        policy.WithOrigins("http://localhost:4200") // Allow Angular app origin
+        policy.WithOrigins("http://localhost:4200", "https://674ec11a06a2450008f88953--clinquant-kataifi-aee780.netlify.app") // Allow Angular app origin
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
