277599c254e5ea739bc0f48615340c81dc124d55
pricope-marius-andrei <pricopemarius33@gmail.com>
2024-12-09 19:00:36 +0200

    remove password required at updating

--- Application/UseCases/Commands/Base/BaseUserCommandValidator.cs
+++ Application/UseCases/Commands/Base/BaseUserCommandValidator.cs
@@ -15,11 +15,6 @@ namespace Application.UseCases.Commands.Base
                 .EmailAddress().WithMessage("Invalid email address.")
                 .MaximumLength(100).WithMessage("Email must not exceed 100 characters.");
 
-            RuleFor(command => command.Password)
-                .NotEmpty().WithMessage("Password is required.")
-                .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
-                .MaximumLength(50).WithMessage("Password must not exceed 50 characters.");
-
             RuleFor(command => command.FirstName)
                 .NotEmpty().WithMessage("First name is required.")
                 .MaximumLength(50).WithMessage("First name must not exceed 50 characters.");
--- Application/UseCases/Commands/Patient/CreatePatientCommandValidator.cs
+++ Application/UseCases/Commands/Patient/CreatePatientCommandValidator.cs
@@ -1,5 +1,6 @@
 ﻿using Application.UseCases.Commands.Base;
 using Domain.Common;
+using FluentValidation;
 
 namespace Application.UseCases.Commands.Patient
 {
@@ -7,7 +8,10 @@ namespace Application.UseCases.Commands.Patient
     {
         public CreatePatientCommandValidator() : base()
         {
-
+            RuleFor(command => command.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+                .MaximumLength(50).WithMessage("Password must not exceed 50 characters.");
         }
     }
 }
