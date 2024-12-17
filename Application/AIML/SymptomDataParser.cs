6866286aca9f8c2c793ebfb2d208adfa10ac0ccc
pricope-marius-andrei <pricopemarius33@gmail.com>
2024-12-17 11:56:05 +0200

    add the ml module

---
+++ Application/AIML/SymptomDataParser.cs
@@ -0,0 +1,44 @@
+﻿using CsvHelper;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+
+namespace Application.AIML
+{
+    public static class SymptomDataParser
+    {
+        public static List<SymptomData> LoadSymptomData(string filePath)
+        {
+            var symptomDataList = new List<SymptomData>();
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Read();
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                   
+                    var newSymptomData = new SymptomData();
+
+                    for (int i = 1; i <= 17; i++) 
+                    {
+                        var symptom = csv.GetField<string>($"Symptom_{i}");
+                        
+                        if (!string.IsNullOrWhiteSpace(symptom)) 
+                        {
+                            typeof(SymptomData).GetProperty($"Symptom_{i}").SetValue(newSymptomData,symptom);
+                        }
+                    }
+
+
+                    var disease = csv.GetField<string>("Disease");
+
+                    newSymptomData.Disease = disease;
+                    symptomDataList.Add(newSymptomData);
+                }
+            }
+            return symptomDataList;
+        }
+
+    }
+}
---
+++ Application/AIML/SymptomDataPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace Application.AIML
+{
+    public class SymptomDataPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string Disease;
+    }
+}
